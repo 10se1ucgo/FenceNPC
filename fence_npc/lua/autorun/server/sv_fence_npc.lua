2e2a512690ccb6d874ca6e6c0a893ee94d23bc67
10se1ucgo <10se1ucgo@users.noreply.github.com>
2016-07-17 16:59:48 -0400

    Fixed potential overwrites over different map names

--- fence_npc/lua/autorun/server/sv_fence_npc.lua
+++ fence_npc/lua/autorun/server/sv_fence_npc.lua
@@ -14,7 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 --]]
-local saved_path = "fence_npc/" .. string.lower(game.GetMap()) .. ".txt"
+local saved_path = function() return "fence_npc/" .. string.lower(game.GetMap()) .. ".txt" end
 local fence_npcs = {}
 local help_msg = "Invalid arguments!\nUse 'fence_npc save' to save all locations.\nUse 'fence_npc delete' to delete all locations.\nUse 'fence_npc respawn' to respawn all locatons.\nUse 'fence_npc reload' to force reload the config file."
 
@@ -46,7 +46,7 @@ function fence_npc_load(ply)
 		file.CreateDir("fence_npc")
 	end
 
-	if not file.Exists(saved_path, "DATA") then
+	if not file.Exists(saved_path(), "DATA") then
 		if ply != nil then
 			ply:PrintMessage(HUD_PRINTCONSOLE, "No Fence NPC locations to respawn!")
 		end
@@ -55,7 +55,7 @@ function fence_npc_load(ply)
 
 	for index, fence_ent in pairs(ents.FindByClass("fence_npc")) do fence_ent:Remove() end
 
-	fence_npcs = util.JSONToTable(file.Read(saved_path, "DATA"))
+	fence_npcs = util.JSONToTable(file.Read(saved_path(), "DATA"))
 	for index, ent in pairs(fence_npcs) do
 		local fence_npc = ents.Create("fence_npc")
 		if IsValid(fence_npc) then
@@ -86,19 +86,19 @@ function fence_npc_save(ply)
 		table.insert(fence_npcs, npc_data)
 	end
 
-	file.Write(saved_path, util.TableToJSON(fence_npcs))
+	file.Write(saved_path(), util.TableToJSON(fence_npcs))
 	print("Saved " .. #fence_npcs .. " NPC locations")
 	ply:PrintMessage(HUD_PRINTCONSOLE, "Saved " .. #fence_npcs .. " NPC locations")
 end
 
 function fence_npc_delete(ply)
 	fence_npcs = {}
-	if not file.Exists(saved_path, "DATA") then
+	if not file.Exists(saved_path(), "DATA") then
 		ply:PrintMessage(HUD_PRINTCONSOLE, "No locations to delete!")
 		return
 	end
 
-	file.Delete(saved_path)
+	file.Delete(saved_path())
 	for num, ent in pairs(ents.FindByClass("fence_npc")) do ent:Remove() end
 	print("Deleted all NPC locations")
 	ply:PrintMessage(HUD_PRINTCONSOLE, "Deleted all NPC locations")
